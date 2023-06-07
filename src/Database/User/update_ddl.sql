b33f1d3838e6df4824da95f1bb550f9369274492
Yanis <yanis.deluca@distrilog.net>
2023-06-07 22:57:18 +0200

    master : modification de l'insertion de la table user

--- src/Database/User/update_ddl.sql
+++ src/Database/User/update_ddl.sql
@@ -2,11 +2,12 @@ PROMPT "07/06/2023 : Création de la table user"
 CREATE TABLE `user` (
   `id` int NOT NULL AUTO_INCREMENT,
   `email` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `roles` int DEFAULT NULL,
+  `roles` json DEFAULT NULL,
   `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `is_active` tinyint(1) DEFAULT '0',
-  `nickname` varchar(64) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `is_active` int NOT NULL,
+  `nickname` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `type` int NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `UNIQ_8D93D649E7927C74` (`email`),
   CONSTRAINT `CHK_user_is_active` CHECK ((`is_active` in (0,1)))
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
